c93bcb9c62441d7441f9fd8594e34859a6694dd7
Italiano <mirandaquezadafabriciojoel@gmail.com>
2024-09-01 21:55:53 -0500

    agregando icono de favoritos

--- Store/src/components/ProductDetails.tsx
+++ Store/src/components/ProductDetails.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Product } from "./Products";
 import Header from "./Header";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeartCirclePlus } from "@fortawesome/free-solid-svg-icons";
 
 export default function ProductDetails() {
     const { id } = useParams<{ id: string }>();
@@ -26,23 +28,41 @@ export default function ProductDetails() {
 
     }, [id])
 
+    const handleAddToFavorites = () => {
+        if (product) {
+            // Obtener productos favoritos actuales del localStorage
+            const favorites = JSON.parse(localStorage.getItem("favorites") || "[]");
+
+            // Agregar el nuevo producto a la lista de favoritos
+            favorites.push(product);
+
+            // Guardar la lista actualizada en el localStorage
+            localStorage.setItem("favorites", JSON.stringify(favorites));
+
+            console.log(`${product.title} agregado a favoritos!`);
+        }
+    };
+
     if (!product) {
         return <div>Cargando...</div>;
     }
 
     return (
         <>
-        <Header/>
-        <main className="w-11/12 mx-auto mt-24">
-            <div className="flex flex-col items-center text-center">
-                <img src={product.img} alt={product.title} className="w-48" />
-                <div className="flex flex-col">
-                    <h1 className="text-lg font-bold">{product.title}</h1>
-                    <p className="mt-4">{product.description}</p>
-                    <p className="text-xl font-bold mt-2 text-primary">S/ {product.price}</p>
+            <Header />
+            <main className="w-11/12 mx-auto mt-24">
+                <div className="flex flex-col items-center text-center">
+                    <img src={product.img} alt={product.title} className="w-48" />
+                    <div className="flex flex-col">
+                        <h1 className="text-lg font-bold">{product.title}</h1>
+                        <p className="mt-4">{product.description}</p>
+                        <div className="flex justify-evenly items-center">
+                            <p className="text-2xl font-bold mt-2 text-primary">S/ {product.price}</p>
+                            <i className="text-xl" onClick={handleAddToFavorites}><FontAwesomeIcon icon={faHeartCirclePlus} /></i>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </main>
+            </main>
         </>
     )
 }
