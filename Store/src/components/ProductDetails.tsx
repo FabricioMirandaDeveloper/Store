e7a64a57f2c40b19e84d7940f939ca7be4eaa161
Italiano <mirandaquezadafabriciojoel@gmail.com>
2024-09-05 21:44:11 -0500

    Integrar react-hot-toast para notificaciones en ProductDetails

--- Store/src/components/ProductDetails.tsx
+++ Store/src/components/ProductDetails.tsx
@@ -4,11 +4,13 @@ import { Product } from "./Products";
 import Header from "./Header";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart, faHeartCirclePlus } from "@fortawesome/free-solid-svg-icons";
+import toast, {Toaster} from "react-hot-toast";
 
 export default function ProductDetails() {
     const { id } = useParams<{ id: string }>();
     const [product, setProduct] = useState<Product | null>(null);
     const [isFavorite, setIsFavorite] = useState(false)
+    const [quantity, setQuantity] = useState(1)
     const apiUrl = import.meta.env.VITE_API_URL;
 
 
@@ -42,12 +44,21 @@ export default function ProductDetails() {
             if (!isFavorite) {
                 favorites.push(product);
                 localStorage.setItem("favorites", JSON.stringify(favorites))
-                setIsFavorite(true) 
+                setIsFavorite(true)
             } else {
                 const updatedFavorites = favorites.filter((fav: Product) => fav.id !== product.id);
                 localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
                 setIsFavorite(false);
             }
+        }
+    }
+
+    const handleAddToCart = () => {
+        if (product) {
+            const cart = JSON.parse(localStorage.getItem('cart') || '{}');
+            cart[product.id] = { ...product, quantity};
+            localStorage.setItem('cart', JSON.stringify(cart));
+            toast.success("Producto Agregado")
         }
     }
 
@@ -66,7 +77,26 @@ export default function ProductDetails() {
                         <p className="mt-4">{product.description}</p>
                         <div className="flex justify-evenly items-center">
                             <p className="text-2xl font-bold mt-2 text-primary">S/ {product.price}</p>
-                            <i className="text-xl" onClick={handleAddToFavorites}><FontAwesomeIcon className="text-primary" icon={isFavorite ? faHeart  : faHeartCirclePlus} /></i>
+                            <i className="text-xl" onClick={handleAddToFavorites}><FontAwesomeIcon className="text-primary" icon={isFavorite ? faHeart : faHeartCirclePlus} /></i>
+                        </div>
+                        <div className="flex justify-around items-center mt-2">
+                            <div>
+                                <button
+                                    onClick={() => setQuantity(prev => (prev > 1 ? prev - 1 : 1))}
+                                    className="bg-primary text-white py-1 px-2 rounded"
+                                >
+                                    -
+                                </button>
+                                <span className="mx-4">{quantity}</span>
+                                <button
+                                    onClick={() => setQuantity(prev => prev + 1)}
+                                    className="bg-primary text-white py-1 px-2 rounded"
+                                >
+                                    +
+                                </button>
+                            </div>
+                            <button onClick={handleAddToCart} className="bg-primary text-white py-1 px-2 rounded">Añadir al carrito</button>
+                            <Toaster/>
                         </div>
                     </div>
                 </div>
